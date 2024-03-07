cd1b32c49bc3b97f39dd04010ea1d0361488ae04
pouyasadri <pouyapaghandeh78@gmail.com>
2024-03-07 15:50:20 +0100

    Initial Commit

---
+++ ComplexAnimation/ComplexAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAnimationApp.swift
+//  ComplexAnimation
+//
+//  Created by Pouya Sadri on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComplexAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComplexAnimation
+//
+//  Created by Pouya Sadri on 07/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
